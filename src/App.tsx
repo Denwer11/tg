9ef1c5c04277d8d42c247c88e07dfc236a3f2288
Denwer1 <denwer0204@gmail.com>
2024-06-25 20:43:48 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
+const botToken = "7429937238:AAHxXYLfQlB3RHTrbO847nNusEQ1RjZ2d7w";
+
+axios
+  .get(`https://api.telegram.org/bot${botToken}/`)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error("Ошибка:", error);
+  });
 
 function App() {
-  const [certificateData, setCertificateData] = useState(null);
-  useEffect(() => {
-    // Получение сертификатов от сервера
-    const fetchCertificates = async () => {
-      try {
-        const response = await axios.get("/api/certificates");
-        setCertificateData(response.data);
-      } catch (error) {
-        console.error("Ошибка получения сертификатов:", error);
-      }
-    };
-    fetchCertificates();
-  }, []);
+  // const [certificateData, setCertificateData] = useState(null);
+  // useEffect(() => {
+  //   // Получение сертификатов от сервера
+  //   const fetchCertificates = async () => {
+  //     try {
+  //       const response = await axios.get("/api/certificates");
+  //       setCertificateData(response.data);
+  //     } catch (error) {
+  //       console.error("Ошибка получения сертификатов:", error);
+  //     }
+  //   };
+  //   fetchCertificates();
+  // }, []);
 
   // const onClose = () => {};
   // const [count, setCount] = useState(0);
