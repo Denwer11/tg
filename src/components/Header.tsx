673369c9c4419c8cd01f601a32f0ba5648b046bf
Denwer1 <denwer0204@gmail.com>
2024-06-26 17:12:32 +0300

    fdfdf

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,11 @@ import Button from "./Button";
 // import "./Header.css";
 
 const Header = () => {
-  const {user, onClose} = useTelegram();
+  const {onClose} = useTelegram();
 
   return (
     <div className={"header"}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={"username"}>{user?.username}</span>
     </div>
   );
 };
